8d655a1cd6b1436433422ae752aa12bd483e75d7
Ramanudeep <49805917+Ramanudeep@users.noreply.github.com>
2019-07-29 11:42:03 +0530

    Update and rename ram1.py to ram2.py

--- ram1.py
+++
@@ -1,7 +0,0 @@
-y=int(input())
-if(y<0):
-  print("Negative")
-elif(y>0):
-  print("Positive")
-else:
-  print("Zero")
---
+++ ram2.py
@@ -0,0 +1,7 @@
+z=int(input())
+if(z>0 and z%2==0):
+  print("Even")
+elif(z>0 and z%2!=0):
+  print("Odd")
+else:
+  print("invalid")
